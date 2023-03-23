22c22b38dc410b72a584758fa0f832d213d82a61
arnarbragi <arnarbb21@ru.is>
2023-03-23 22:42:59 +0000

    main fix :sake: :chestnut: :chestnut:

--- project2/main.py
+++ project2/main.py
@@ -1,6 +1,7 @@
 from environment import Environment
 import os
 import time
+import platform
 
 
 def main():
@@ -41,8 +42,8 @@ def dif_menu():
 
 def alg_menu():
     os.system('cls')
-    algorithms = [0, "Backtrack", "FwdCheck"]
-    print("\n----Choose an algortihm----\n1) Backtracking (Brute force)\n2) Backtracking with forward checking\n0) Go back")
+    algorithms = [0, "BacktrackBF", "Backtrack", "FwdCheck"]
+    print("\n----Choose an algortihm----\n1) Backtracking (Brute force)\n2) Backtracking\n3) Backtracking with forward checking\n0) Go back")
     choice = input("Enter option: ")
 
     try:
@@ -53,10 +54,15 @@ def alg_menu():
         return
 
 
-def heu_menu():
+def heu_menu(alg_choice):
     os.system('cls')
-    heuristics = [0, "MRV", "MRVDegree", "Random", "None"]
-    print("\n----Choose a heuristic----\n1) MRV\n2) MRV with degree\n3) Random\n4) None\n0) Go back")
+    if alg_choice == "Backtrack":
+        heuristics = [0, "MRV", "MRVDegree", "None"]
+        print("\n----Choose a heuristic----\n1) MRV\n2) MRV with degree\n3) None\n0) Go back")
+    elif alg_choice == "FWDCheck":
+        heuristics = [0, "MRV", "None"]
+        print("\n----Choose a heuristic----\n1) MRV\n2) None\n0) Go back")
+    
     choice = input("Enter option: ")
 
     try:
@@ -92,13 +98,46 @@ if __name__ == "__main__":
                 if alg_choice == 0:
                     difficulty = None
                     break
+                elif alg_choice == "BacktrackBF":
+                    break
                 heu_choice = None
                 while heu_choice == None:
-                    heu_choice = heu_menu()
+                    heu_choice = heu_menu(alg_choice)
                     if heu_choice == 0:
                         alg_choice = None
                         break
 
-    print(size, difficulty, alg_choice, heu_choice)
+    
+    if heu_choice != None or alg_choice == "BacktrackBF":
+        dif_choose = {"Easy": [7, 38, 118], "Medium": [6, 32, 100], "Hard": [5, 25, 77]}
+        indexes = {4: 0, 9: 1, 16: 2}
 
+        env = Environment(size, dif_choose[difficulty][indexes[size]])
+        env.current_state.get_board()
 
+        os.system('cls')
+        print(env.current_state)
+        print()
+
+        if alg_choice == "BacktrackBF":
+            env.backtracking_brute()
+
+        elif alg_choice == "Backtrack":
+            if heu_choice == "MRV":
+                env.backtracking_mrv()
+            elif heu_choice == "MRVDegree":
+                env.backtracking_mrv_deg()
+            elif heu_choice == "None":
+                env.backtracking()
+
+        elif alg_choice == "FwdCheck":
+            if heu_choice == "None":
+                env.backtracking_forward_check()
+            elif heu_choice == "MRV":
+                env.backtracking_forward_check_mrv()
+            
+    """if platform.system() == "Windows":
+        os.system("pause")
+    else:
+        os.system("/bin/bash -c 'read -s -n 1 -p \"Press any key to continue...\"'")
+        print()"""

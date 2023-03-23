488307a2944709e48b05b7f75c30ee673996e392
halli21 <haraldur.jon.fridriksson@gmail.com>
2023-03-23 16:25:09 +0000

    our q

--- project2/agent.py
+++ project2/agent.py
@@ -1,6 +1,7 @@
 import math
 import heapq
 
+from mrv_queue import MRV_Queue, MRV_Node
 from copy import deepcopy
 
 
@@ -11,7 +12,7 @@ class Search:
         self.block_size = int(math.sqrt(size))
         self.board = board
         self.domains = domains
-        self.mrv_queue = []
+        self.mrv_queue = MRV_Queue()
         
 
 
@@ -183,26 +184,30 @@ class Search:
 
 
 
+
+
 # ------------- MRV Heuristic implemented
 
+    """
     def update_mrv_value(self, old_y, old_x, new_mrv_value):
-        new_tuple = (new_mrv_value, old_y, old_x)
+        node = MRV_Node(new_mrv_value, old_y, old_x)
         new_queue = []
         updated = False
 
         while len(self.mrv_queue) > 0:
             item = heapq.heappop(self.mrv_queue)
             if item[1] == old_y and item[2] == old_x:
-                heapq.heappush(new_queue, new_tuple)
+                heapq.heappush(new_queue, node)
                 updated = True
             else:
                 heapq.heappush(new_queue, item)
 
         if not updated:
-            heapq.heappush(new_queue, new_tuple)
+            heapq.heappush(new_queue, node)
 
         self.mrv_queue = new_queue
         heapq.heapify(self.mrv_queue)
+    """
 
 
     def reduce_domains_value_mrv(self, value, ycord, xcord):
@@ -214,18 +219,17 @@ class Search:
                 if len(self.domains[ycord][x]) > 1:
                     self.domains[ycord][x].remove(value)
                     if self.board[ycord][x] == " ":
-                        self.update_mrv_value(ycord, x, len(self.domains[ycord][x]))
+                        self.mrv_queue.update_node(ycord, x, len(self.domains[ycord][x]))
                 else:
                     flag = False
 
         # reduce domain of cells in the same column
         for y in range(self.size):
             if y != ycord and value in self.domains[y][xcord]:
-               
                 if len(self.domains[y][xcord]) > 1:
                     self.domains[y][xcord].remove(value)
                     if self.board[y][xcord] == " ":
-                        self.update_mrv_value(y, xcord, len(self.domains[y][xcord]))
+                        self.mrv_queue.update_node(y, xcord, len(self.domains[y][xcord]))
                 else:
                     flag = False
         
@@ -237,7 +241,7 @@ class Search:
                     if len(self.domains[x][y]) > 1:
                         self.domains[x][y].remove(value)
                         if self.board[x][y] == " ":
-                            self.update_mrv_value(x, y, len(self.domains[x][y]))
+                            self.mrv_queue.update_node(x, y, len(self.domains[x][y]))
                     else:
                         flag = False
         return flag
@@ -247,27 +251,27 @@ class Search:
                              
     def backtracking_search_mrv(self, expansions = 0):
 
-        if not self.mrv_queue:
+        if self.mrv_queue.size == 0:
             return True, expansions
         
-        mrv_value, y, x = heapq.heappop(self.mrv_queue)
+        node = self.mrv_queue.pop()
 
-        domain_list = deepcopy(self.domains[y][x])
+        domain_list = deepcopy(self.domains[node.ycord][node.xcord])
 
         for num in domain_list:
-            if self.checkIfSafe(y, x, num):
+            if self.checkIfSafe(node.ycord, node.xcord, num):
                 expansions += 1
-                self.board[y][x] = num
+                self.board[node.ycord][node.xcord] = num
                 temp = deepcopy(self.domains)
-                temp_mrv = deepcopy(self.mrv_queue)
-                forward_check = self.reduce_domains_value_mrv(num, y, x)
+                #temp_mrv = deepcopy(self.mrv_queue)
+                forward_check = self.reduce_domains_value_mrv(num, node.ycord, node.xcord)
                 success, expansions = self.backtracking_search_mrv(expansions)
                 if success:
                     return True, expansions
                 
-                self.mrv_queue = temp_mrv
+                #self.mrv_queue = temp_mrv
                 self.domains = temp
-                self.board[y][x] = " "
+                self.board[node.ycord][node.xcord] = " "
         return False, expansions
 
     
